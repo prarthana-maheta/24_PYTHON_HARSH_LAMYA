19007123ad5f3c5fa8dc2cab430e79d998c44b3f
prarthana-maheta <142563768+prarthana-maheta@users.noreply.github.com>
2024-07-12 20:16:57 +0530

    Add files via upload

---
+++ jul_3.py
@@ -0,0 +1,47 @@
+'''print("Hello world")
+
+int1 ,int2 = 1,2
+int1,int2 = int2,int1'''
+
+# doc string 
+# int1=1
+# int2=2
+# int3=3
+#print(int1 ,int2 )
+
+
+# data types
+
+# base data 
+
+# interger  -- number -- no fix length - non iterable - no mkethods no functions
+
+# a = 123456789012345678901234567890
+# b=2
+
+# print(a>b)
+
+# float - . values - 4.1234567890,5.6 - non iterable - no mkethods no functions
+# complex - 1j+1 -- complex can only be created - can't be converted 
+
+
+# b= '1j+1'
+# a = 1j+1 
+
+# j= 1
+# a = 1j+1 
+# print(a)
+
+# - non iterable - no mkethods no functions
+# a=1.23456789
+# b=6
+# a=5.9
+# print(type(b))
+# print(float(b))
+# print(int(a))
+
+# string -char -'' or "1234567890-cvbnm,.!@#$%^&*()         " -- iterable
+
+
+# 123456789012345 -67890
+# string -- 
---
+++ july_12.py
@@ -0,0 +1,77 @@
+# string 
+# data type --- collections --class, functions, methods
+
+# immutable -- cant be changed
+# string -- immutable, iterable
+#  --- ordered 
+# --- 
+
+str1 = 'Royal'
+# str2 = str()
+# str1 = 'Royal cg road'
+# 
+# str1[3] = 't'
+# print(str2)
+
+# int1 = 1234
+# # str1 ='123b'
+# print(type(str(int1)))
+
+# indexing 
+# slicing 
+# functions 
+# methods
+str1 = 'I am a Royal student, I also love Python'
+    #  ----0,1,2,3,-----------20    
+    #     -21             ,-----------        -1--   
+# 2 type of index -- +ve & -ve index 
+# each and every element has 2 index 
+
+# print(str1[-7])
+# [start: end : step/skip]
+# slic = str1[5:-28:-1]
+# print(slic)
+
+# Royal student
+# Royal Python
+# reverse of the str1
+
+# functions 
+# print()
+# type()
+
+# print(len(str1))
+
+# methods 
+# case 
+# casing methods of strings 
+
+str1 = 'I am aa Royal student, I also love Python ROYAL'
+
+
+# print(str1[7:].upper())
+# print(str1.upper())
+# print(str1.lower())
+# print(str1.capitalize())
+# print(str1.title())
+# print(str1.swapcase())
+
+
+# counting methods 
+
+# print(str1.upper().count('Y', 18))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
